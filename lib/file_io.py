86ca72d6e0b4d8bd889a26ae93ec13b34bbbabc7
Kelvin Kipchumba <chogekelvin90@gmail.com>
2025-01-16 14:35:15 +0300

    file i/o

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
+    #file_name = 'file.txt'
+    file_content = "This is a test content."
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    #file_name = 'file.txt'
+    append_content = "\nAppended content."
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt',mode='r', encoding='utf-8') as text_file:
+        content = text_file.read()
+        return content
     pass
